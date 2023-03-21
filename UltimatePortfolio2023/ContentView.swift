7a8f36c9f7ad5d61ea0de7a2f16c2de67f2d9f30
Sam H <samh007@tutanota.com>
2023-03-21 18:02:21 +0900

    Addition of contentview, delete method synchronizing issues

--- UltimatePortfolio2023/ContentView.swift
+++ UltimatePortfolio2023/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @EnvironmentObject var dataController: DataController
+    
+    var issues: [Issue] {
+        let filter = dataController.selectedFilter ?? .all
+        var allIssues: [Issue]
+        
+        if let tag = filter.tag {
+            allIssues = tag.issues?.allObjects as? [Issue] ?? []
+        } else {
+            let request = Issue.fetchRequest()
+            request.predicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
+            allIssues = (try? dataController.container.viewContext.fetch(request)) ?? []
         }
-        .padding()
+        
+        return allIssues.sorted()
+    }
+    
+    var body: some View {
+        List {
+            ForEach(issues) { issue in
+                IssueRow(issue: issue)
+            }
+            .onDelete(perform: delete)
+        }
+        .navigationTitle("Issues")
+    }
+    
+    func delete(_ offsets: IndexSet) {
+        for offset in offsets {
+            let item = issues[offset]
+            dataController.delete(item)
+        }
     }
 }
 
---
+++ UltimatePortfolio2023/IssueRow.swift
@@ -0,0 +1,52 @@
+//
+//  IssueRow.swift
+//  UltimatePortfolio2023
+//
+//  Created by Sam Hui on 2023/03/21.
+//
+
+import SwiftUI
+
+struct IssueRow: View {
+    @EnvironmentObject var dataController: DataController
+    @ObservedObject var issue: Issue
+
+    var body: some View {
+        NavigationLink(value: issue) {
+            HStack {
+                Image(systemName: "exclamationmark.circle")
+                    .imageScale(.large)
+                    .opacity(issue.priority == 2 ? 1 : 0)
+                
+                VStack(alignment: .leading) {
+                    Text(issue.issueTitle)
+                        .font(.headline)
+                        .lineLimit(1)
+                    
+                    Text("No tags")
+                        .foregroundStyle(.secondary)
+                        .lineLimit(1)
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text(issue.issueCreationDate.formatted(date: .numeric, time: .omitted))
+                        .font(.subheadline)
+                    
+                    if issue.completed {
+                        Text("CLOSED")
+                            .font(.body.smallCaps())
+                    }
+                }
+                .foregroundStyle(.secondary)
+            }
+        }
+    }
+}
+
+struct IssueRow_Previews: PreviewProvider {
+    static var previews: some View {
+        IssueRow(issue: .example)
+    }
+}
